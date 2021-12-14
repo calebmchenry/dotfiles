001302cee5eeeacb9d97849b7e0f63b66b93d928
calebmchenry <calebmchenry@gmail.com>
2021-12-13 20:07:51 -0500

    update neovim to 0.6 with LSP

---
+++ home/.config/nvim/plugins.vim
@@ -0,0 +1,29 @@
+
+call plug#begin()
+
+" Theme
+Plug 'tomasiser/vim-code-dark'
+
+" LSP
+Plug 'neovim/nvim-lspconfig'
+
+" Auto Completion
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+Plug 'nvim-lua/completion-nvim'
+
+" Search
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+
+" Fuzzy Finder
+Plug 'airblade/vim-rooter'
+
+call plug#end()
+
+let $FZF_DEFAULT_COMMAND = 'rg --files --hidden'
---
+++ home/.hyper.js
@@ -0,0 +1,25 @@
+module.exports = {
+  config: {
+		fontFamily: '"Fira Code", monospace',
+
+		colors: {
+      black: '#000000',
+      red: '#ff3b30',
+      green: '#4cd964',
+      yellow: '#ffcc00',
+      blue: '#006fff',
+      magenta: '#fb4dca',
+      cyan: '#5ad3fa',
+      white: '#ffffff',
+      lightBlack: '#808080',
+      lightRed: '#ff3b30',
+      lightGreen: '#4cd964',
+      lightYellow: '#ffcc00',
+      lightBlue: '#006fff',
+      lightMagenta: '#fb4dca',
+      lightCyan: '#5ad3fa',
+      lightWhite: '#ffffff'
+    },
+  },
+};
+
